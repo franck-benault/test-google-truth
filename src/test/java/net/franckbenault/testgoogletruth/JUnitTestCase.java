edb6cd6570535de5f94c8a796702fd5dcf7bfea0
franck-benault <franck-benault@orange.fr>
2015-06-03 17:32:00 +0200

    complete add named option

--- src/test/java/net/franckbenault/testgoogletruth/JUnitTestCase.java
+++ src/test/java/net/franckbenault/testgoogletruth/JUnitTestCase.java
@@ -12,14 +12,19 @@ public class JUnitTestCase {
 	public void t01AssertArrayEquals() {
 		int[] ints1 = {1,3};
 		int[] ints2 = {1,3};
+		
 		Truth.assertThat(ints1).isEqualTo(ints2);
+		Truth.assertThat(ints1).named("int tab").isEqualTo(ints2);
+		
 	}
 	
 	@Test
 	public void t02aAssertEquals() {
 		String val1 = "String";
 		String val2 = "String";
+		
 		Truth.assertThat(val1).isEqualTo(val2);
+		Truth.assertThat(val1).named("value 1").isEqualTo(val2);
 
 	}
 	
@@ -27,27 +32,26 @@ public class JUnitTestCase {
 	public void t02bAssertNotEquals() {
 		String val1 = "String1";
 		String val2 = "String2";
+		
 		Truth.assertThat(val1).isNotEqualTo(val2);
-
+		Truth.assertThat(val1).named("first value").isNotEqualTo(val2);
 	}
-	
-
-	// assertFalse
-	//assertNull / assertNotNull
-	//assertSame /assertNotSame
 	
 	@Test
 	public void t03aAssertTrue() {
 		
 		boolean exp = true;
+		
 		Truth.assertThat(exp).isTrue();
-;
+		Truth.assertThat(exp).named("expression").isTrue();
 	}
 	
 	@Test
 	public void t03bAssertFalse() {
 		boolean exp = false;
+		
 		Truth.assertThat(exp).isFalse();
+		Truth.assertThat(exp).named("expression").isFalse();
 	}
 	
 	@Test
@@ -56,6 +60,7 @@ public class JUnitTestCase {
 		Object obj = null;
 				
 		Truth.assertThat(obj).isNull();
+		Truth.assertThat(obj).named("my object ").isNull();
 	}
 	
 	@Test
@@ -63,6 +68,7 @@ public class JUnitTestCase {
 		Object obj = new Object();
 		
 		Truth.assertThat(obj).isNotNull();
+		Truth.assertThat(obj).named("my object").isNotNull();
 	}
 	
 	@Test
@@ -70,7 +76,7 @@ public class JUnitTestCase {
 		Object obj = new Object();
 		
 		Truth.assertThat(obj).isSameAs(obj);
-
+		Truth.assertThat(obj).named("my object").isSameAs(obj);
 	}
 	
 	@Test
@@ -79,6 +85,8 @@ public class JUnitTestCase {
 		Object obj2 = new Object();
 		
 		Truth.assertThat(obj1).isNotSameAs(obj2);
+		Truth.assertThat(obj1).named("my object ").isNotSameAs(obj2);
+		
 
 	}
 }
